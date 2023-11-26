78dcc042ae45a57aba4b5e1ae9e573e244b3db9c
sadath <sadath@orchids.edu.in>
2023-11-26 15:48:34 +0530

    migrations issue fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 from fastapi import FastAPI
-# from app.api.v1 import user
+from app.api.v1 import user as user_apis
 from medcare_app.dependencies.database import engine
 from medcare_app.models import user as user_models
 
@@ -14,5 +14,5 @@ app = FastAPI(
 )
 
 
-# app.include_router(user.router)
+app.include_router(user_apis.router)
 user_models.Base.metadata.create_all(bind=engine)
--- medcare_app/dependencies/database.py
+++ medcare_app/dependencies/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://med_care:sadath777@34.135.29.238:5432/med_care_bglr"
+DATABASE_URL = "postgresql://med_care:sadath777@34.135.29.238:5432/medcare_app_db"
 
 
 engine = create_engine(DATABASE_URL, connect_args={}, future=True)
